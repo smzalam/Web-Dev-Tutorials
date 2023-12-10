4b4e93173ae967532fad35154743786ea9513478
smzalam <alam.syedmz@gmail.com>
2023-12-09 21:13:13 -0500

    removed hello-world application

--- hello-world/app/products/[productId]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProductDetails(
-    { params }: {
-        params: {
-            productId: string
-        }
-    }
-) {
-   return <h1>Details about Product {params.productId}</h1> 
-}
