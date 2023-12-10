d8d15309767fe9a12d5d70cc10951a35259f9cb7
smzalam <alam.syedmz@gmail.com>
2023-12-09 21:12:36 -0500

    added condevolution's nextjs tutorial

---
+++ hello-world/app/products/[productId]/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductDetails(
+    { params }: {
+        params: {
+            productId: string
+        }
+    }
+) {
+   return <h1>Details about Product {params.productId}</h1> 
+}
